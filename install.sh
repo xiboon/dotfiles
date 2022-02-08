653d97d318e8fa6fa338aadedd9db053023ae897
xiboon <38988733+xiboon@users.noreply.github.com>
2022-02-08 13:17:24 +0100

    i hope

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 path="$HOME/.dotfiles"
-ln -sf "$PATH".zshrc "$HOME"/.zshrc
+ln -sf "$path"/zshrc "$HOME"/.zshrc
 mkdir -p $HOME/.config
 ln -sf "$path"/.config/htop "$HOME"/.config/htop
 ln -sf "$path"/.config/neofetch "$HOME"/.config/neofetch
