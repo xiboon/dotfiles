e3cab1c221698bcd3a25550b82dae10dde8d3a7a
xiboon <38988733+xiboon@users.noreply.github.com>
2022-02-08 13:29:51 +0100

    IDK

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 path="$HOME/.dotfiles"
 ln -sf "$path"/.zshrc "$HOME"/.zshrc
 mkdir -p $HOME/.config
-ln -sf "$path"/.config/htop "$HOME"/.config/htop
+ln -sf "$path"/.config/htop/htoprc "$HOME"/.config/htop/htoprc
 ln -sf "$path"/.config/neofetch "$HOME"/.config/neofetch
-ln -sf "$path"/.config/alacritty "$HOME"/.config/alacritty
+ln -sf "$path"/.config/alacritty/alacritty.conf "$HOME"/.config/alacritty/alacritty.conf
