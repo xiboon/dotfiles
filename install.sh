bfedaf186ba18af8f63795b7f4a8fabd1128a9ac
xiboon <38988733+xiboon@users.noreply.github.com>
2022-02-08 13:34:09 +0100

    h

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 path="$HOME/.dotfiles"
 ln -sf "$path"/.zshrc "$HOME"/.zshrc
 mkdir -p $HOME/.config
+mkdir -p $HOME/.config/alacritty
 ln -sf "$path"/.config/htop/htoprc "$HOME"/.config/htop/htoprc
+mkdir -p $HOME/.config/neofetch
 ln -sf "$path"/.config/neofetch "$HOME"/.config/neofetch
+mkdir -p $HOME/.config/alacritty
 ln -sf "$path"/.config/alacritty/alacritty.conf "$HOME"/.config/alacritty/alacritty.conf
